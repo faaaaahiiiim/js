f8a7d2b8cf910d7e1716ed0e8d6d3e1c57a028f0
faaaaahiiiim <mhd.fahiimfaisal@gmail.com>
2022-07-11 14:58:13 +0400

    changed

--- DAY-26/app.js
+++ DAY-26/app.js
@@ -1,5 +1,19 @@
-console.log(window.innerWidth)
-console.log(window.innerHeight)
-window.alert("this is an alert")
-const name = window.prompt("sup?")
-console.log(window)
+// console.log(window.innerWidth)
+// console.log(window.innerHeight)
+// window.alert("this is an alert")
+// const name = window.prompt("sup?")
+
+// console.log(window)
+
+const car = {
+    make: "BMW",
+    model: "x4",
+    move: function(){
+        // console.log(this)
+        console.log("moving")
+    }
+}
+
+car.move()
+console.log(this)
+
