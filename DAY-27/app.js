ccabb3f02a8f5c729f091125344753c7d2e2bea0
faaaaahiiiim <mhd.fahiimfaisal@gmail.com>
2022-07-15 17:07:20 +0400

    new

---
+++ DAY-27/app.js
@@ -0,0 +1,44 @@
+// //function with es6
+// const greet = function (name="john"){
+//     console.log("hi " + name);
+// } //never hoisted at the top
+
+// function greet2 (name="john"){
+//     console.log("hi " + name);
+// }
+
+// greet()
+// greet2()
+
+
+
+
+// //normal function
+// const add = function (a=0, b=0){
+//     return a+b;
+// }
+// console.log(add(1,4))
+
+// //function using arrow aka arrow function
+// const addArrow = (a=0 ,b=0) => {
+//     return a+b;
+// }
+// console.log(addArrow(1,2))
+
+// //      or (shorter)
+// const addArrow2 = (a=0, b=0) => a+b;
+// console.log(addArrow2(5,5))
+
+// //filter using arrow function
+// const arr= [1,2,3,4,5,6];
+// const newArr = arr.filter((item) => item%2 == 0)
+// console.log(newArr)
+
+// //map using arrow function
+// const arr2= arr.map((item) => item+2)
+// console.log(newArr)
+
+// //reduce using arrow function
+// const sum= arr.reduce((a,b) => a+b)
+// console.log(sum) 
+
